1a9b08f8f42a5521381435b647bc3a7cb2014c39
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-11-19 22:40:17 -0800

    Checking everything

--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@
 #include "Graph.h"
 using namespace std;
 
+// Constants
+const int MIN_CHOICE = 0;
+const int MAX_CHOICE = 5;
+
 // Function prototypes
 void displayMenu();
 
@@ -38,65 +42,143 @@ int main()
     // Creates graph
     Graph graph(edges);
 
-    // Prints adjacency list representation of graph
-    graph.printGraph();
-    
-    // Create a starting vertex
-    int start = 0;
-
-    // DFS implementation
-    graph.dfs(start);
-    cout << endl;
-
-    // BFS implementation
-    graph.bfs(start);
-    cout << endl;
-
-    // Create a vector the store the shortest paths
-    int source = 0;
-    vector<int> shortestPaths = graph.djikstra(graph.adjList, source);
-
-    // Display a message
-    cout << "Shortest path from " << graph.locationNames[source] << "(" << source << "): ";
-    cout << endl;
-
-    // Display all the shortest paths
-    for (int i = 0; i < shortestPaths.size(); i++)
+    // Create a loop for the operations
+    while (true)
     {
-        // Display the distances
-        cout << " --> " << graph.locationNames.at(i) << "(" << i << "): ";
-        cout << shortestPaths.at(i) << "m" << endl;
+        // Create a variable to hold the user's choice
+        int choice = 0;
+
+        // Display the menu
+        displayMenu();
+
+        // Prompt the user to enter their choice
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        // Validate the input
+        while (choice < MIN_CHOICE || choice > MAX_CHOICE)
+        {
+            // Display an error message
+            cout << "Invalid! Please enter a valid choice (0-5)!" << endl;
+
+            // Display the menu again
+            displayMenu();
+
+            // Prompt the user to enter a new choice
+            cout << "Enter your choice: ";
+            cin >> choice;
+        }
+
+        // Create a starting vertex
+        int start = 0;
+
+        // Switch cases for the choice
+        switch(choice)
+        {
+            // Display the adjacency list
+            case 1: 
+            {
+                // Call the printGraph() function
+                graph.printGraph();
+
+                break;
+            }
+
+            // Implement BFS from vertex 0
+            case 2:
+            {
+                // Call the bfs() function
+                graph.bfs(start);
+                cout << endl;
+
+                break;
+            }
+
+            // Implement DFS from vertex 0
+            case 3:
+            {
+                // Call the dfs() function
+                graph.dfs(start);
+                cout << endl;
+
+                break;
+            }
+
+            // Shortest Paths from vertex 0
+            case 4:
+            {
+                // Create a vector the store the shortest paths and call djikstra() function
+                vector<int> shortestPaths = graph.djikstra(graph.adjList, start);
+
+                // Display a message
+                cout << "Shortest path from " << graph.locationNames[start] << "(" << start << "): ";
+                cout << endl;
+
+                // Display all the shortest paths
+                for (int i = 0; i < shortestPaths.size(); i++)
+                {
+                    // Display the distances
+                    cout << " --> " << graph.locationNames.at(i) << "(" << i << "): ";
+                    cout << shortestPaths.at(i) << "m" << endl;
+                }
+
+                // Enter a new line
+                cout << endl;
+            }
+
+            // Minimum Spanning Tree
+            case 5:
+            {
+                // Create a new vector to store the minimum spanning tree
+                vector<Edge> mst = graph.kruskal(edges);
+
+                // Display the minimum spanning tree
+                cout << "Jurassic Park Minimum Spanning Tree For All Locations:" << endl;
+                for (int i = 0; i < mst.size(); i++)
+                {
+                    // Display the Edge
+                    cout << " -->";
+                    cout << "From " << graph.locationNames.at(mst.at(i).src) << " (" << mst.at(i).src << ") ";
+                    cout << "To " << graph.locationNames.at(mst.at(i).dest) << " (" << mst.at(i).dest << "): ";
+                    cout << mst.at(i).weight << "m" << endl;
+                }
+
+                // Enter a new line
+                cout << endl;
+            }
+
+            // Exit
+            case 0:
+            {
+                // Display a message
+                cout << "Thank you for using the program! Exiting..." << endl;
+
+                // Exit the main function
+                return;
+            }
+        }
     }
-
-    // Enter a new line
-    cout << endl;
-
-    // Create a new vector to store the minimum spanning tree
-    vector<Edge> mst = graph.kruskal(edges);
-
-    // Display the minimum spanning tree
-    cout << "Jurassic Park Minimum Spanning Tree For All Locations:" << endl;
-    for (int i = 0; i < mst.size(); i++)
-    {
-        // Display the Edge
-        cout << " -->";
-        cout << "From " << graph.locationNames.at(mst.at(i).src) << " (" << mst.at(i).src << ") ";
-        cout << "To " << graph.locationNames.at(mst.at(i).dest) << " (" << mst.at(i).dest << "): ";
-        cout << mst.at(i).weight << "m" << endl;
-    }
-
-    // Enter a new line
-    cout << endl;
 
     return 0;
 }
 
 // Function implementations
+/*
+    displayMenu()
+    Display the menu of operations on the Jurassic Park locations graph
+    Arguments: none
+    Return: none
+*/
 void displayMenu()
 {
     // Display a header
     cout << "Jurassic Park Locations Network Menu:" << endl;
 
     // Display all the options
-    cout << "[1] "
+    cout << "[1] Display Jurassic Park Locations Network" << endl;
+    cout << "[2] Explore Park (BFS)" << endl;
+    cout << "[3] Go On Deep Exploration (DFS)" << endl;
+    cout << "[4] Calculate Shortest Paths From Main Gate" << endl;
+    cout << "[5] Find Minimum Spanning Tree of All Locations" << endl;
+    cout << "[0] Exit" << endl;
 }
